cced183b6c5afe9721677ad140def150354e8f74
Justin Yu <lipohshun1@gmail.com>
2019-08-05 22:11:45 +0800

    format

---
+++ SQL/SQL/format.sql
@@ -0,0 +1,8 @@
+set linesize 300;
+set pagesize 300;
+column payment_no heading "Payment No.";
+column amount heading "Amount";
+column account_no heading "Account No.";
+column amount format $9,999.00;
+column payment_no format A11;
+column account_no format A11;
